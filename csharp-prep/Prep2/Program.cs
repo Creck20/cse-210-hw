b436e71fbb318493adaa608ff943aa872019bd22
Caeson Reckling <pec22015@byui.edu>
2024-04-27 22:10:18 -0600

    Finished Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter your grade as a percentage: ");
+        string user_input = Console.ReadLine();
+        int percentage = int.Parse(user_input);
+
+        bool pass = true;
+        string grade = "A";
+
+        if (percentage >= 90)
+        {
+            grade = "A";
+        }
+        else if (percentage >= 80)
+        {
+            grade = "B";
+        }
+        else if (percentage >= 70)
+        {
+            grade = "C";
+        }
+        else if (percentage >= 60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+        if (percentage > 70)
+        {
+            pass = true;
+        }
+        else
+        {
+            pass = false;
+        }
+        if (pass)
+        {
+            Console.WriteLine("Congratulations, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry you did not pass! Better luck next time!");
+        }
+        Console.WriteLine($"You got a {grade}.");
     }
 }
