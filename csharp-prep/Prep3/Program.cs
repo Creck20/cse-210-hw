dde6a457360be8e8f5475674688bd6a000254f12
Caeson Reckling <pec22015@byui.edu>
2024-04-29 23:06:28 -0600

    Finished Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Initialize magic number: 
+        Random radnomGenerator = new Random();
+        int magic_number = radnomGenerator.Next(1,100);
+
+        //Intialize guess:
+        Console.WriteLine("Guess the magic number!");
+        int guess = -1;
+
+        //Run unitl guessed: 
+        while (guess != magic_number)
+        {
+
+        Console.Write("What is your guess? ");
+        guess = int.Parse(Console.ReadLine());
+
+        if (guess > magic_number)
+        {
+            Console.WriteLine("Lower");
+        }
+        else if (guess < magic_number)
+        {
+            Console.WriteLine("Higher");
+        }
+        else
+        {
+            Console.WriteLine("Correct!");
+        }
+
+        }
     }
 }
