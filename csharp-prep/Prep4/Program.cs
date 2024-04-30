e81875c5396aae67e100ff7611e62635e946ede8
Caeson Reckling <pec22015@byui.edu>
2024-04-30 15:42:58 -0600

    Finished Prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Please enter a series of positve integers. Stop by entering 0.");
+        int number = 1;
+        List<int> numbers = new List<int>();
+
+        while (number != 0)
+        {
+            Console.Write("Please enter a positive integer: ");
+            number = int.Parse(Console.ReadLine());
+            if (number > 0)
+            {
+                numbers.Add(number);
+            }
+        }
+        foreach (int i in numbers)
+        {
+            Console.WriteLine(i);
+        }
+
+        int summation = numbers.Sum();
+        Console.WriteLine($"The sum is: {summation}.");
+
+        int length = numbers.Count;
+        Console.WriteLine($"The length is: {length}.");
+
+        int average = summation / length;
+        Console.WriteLine($"The average is: {average}.");
+
+        int max = numbers.Max();
+        Console.WriteLine($"The max is: {max}.");
+
+        int min = numbers.Min();
+        Console.WriteLine($"The min is: {min}.");
     }
 }
