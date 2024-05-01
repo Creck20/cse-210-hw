2f8ade7e34051ee77cf9c203f16d11ea809d518e
Caeson Reckling <pec22015@byui.edu>
2024-05-01 11:43:12 -0600

    Finished Prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
-    }
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+        static string PromptUserName()
+        {
+            Console.Write("Please enter a username: ");
+            string userName = Console.ReadLine();
+            return(userName);
+        }
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            int userNumber = int.Parse(Console.ReadLine());
+            return(userNumber);
+        }
+        static int SquareNumber(int favorite)
+        {
+            return(favorite*favorite);
+        }
+        static void DisplayResult(int userNumber, string userName)
+        {
+            Console.WriteLine($"Username: {userName}");
+            Console.WriteLine($"User-number: {userNumber}");
+        }
+
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int squaredNumber = SquareNumber(userNumber);
+        DisplayResult(squaredNumber,userName);
+
+    }     
 }
