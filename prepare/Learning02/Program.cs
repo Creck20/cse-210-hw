1ea8d80eadb09bc62f24db8dce8bad2febc8650c
Caeson <pec22015@byui.edu>
2024-05-06 11:10:07 -0600

    Completed Learning Activity 2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Peterson and Sons and Friends Bits and Parts";
+        job1._jobTitle = "Bit Maker";
+        job1._startYear = 1998;
+        job1._endYear = 2024;
+
+        Job job2 = new Job();
+        job2._company = "Gorb Co.";
+        job2._jobTitle = "Globulous";
+        job2._startYear = 1950;
+        job2._endYear = 2024;
+
+        Resume resume1 = new Resume();
+        resume1._name = "Doctor Dankmaster";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+       resume1.Display();
     }
 }
